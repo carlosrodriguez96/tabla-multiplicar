923987f4ac3edd2b2d7f312a60436739fbff4715
carlosrodriguez96 <esemismo96@gmail.com>
2017-03-16 10:01:05 -0500

    Update mi-funcion.js

--- js/mi-funcion.js
+++ js/mi-funcion.js
@@ -1,1 +1,30 @@
 
+var acumuladorApp = angular.module( 'acumuladorApp', [] );
+        
+        acumuladorApp.controller( "acumuladorAppCtrl",
+            
+            //[ "$scope",  //Originalmente solo se minificaba el scope. Esta linea también funciona en lugar de la de abajo.
+            [ "$scope", "$http", //Esto es para minificar, pero interfiere con lo de php, hay que minificar las otras variables.
+             
+                function( $scope, $http )
+                {                    
+                    
+
+                     $scope.cargar_datos = function()
+                    {	var numero=document.getElementById('numero').value;
+                    console.log(numero);
+                    var result="";
+
+                    
+                       for (var i =0; i <= 12; i++) {
+                      
+                       	var resultado=numero*i;
+                       	var result=result+" "+resultado;
+                       	$scope.numdi=numero;
+                       	$scope.resultado=result;
+                       }
+						
+                    };
+                }
+            ] //Si se minifica, se deben minificar todas las llamadas inyecciones, de lo contrario mejor no minifique nada.
+        );
